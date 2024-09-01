b0af21b4db4d2139fb140e8ed349449f6495e27b
cnFuwa <cnliufu@outlook.com>
2024-09-01 08:41:37 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -9,22 +9,32 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.use(cors({optionsSuccessStatus: 200})); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+	res.sendFile(__dirname + '/views/index.html');
 });
 
 // your first API endpoint...
+app.get('/api/whoami', function (req, res) {
+	const headers = req.headers;
+	const ip = headers.host;
+	console.log(headers.origin);
+	console.log(headers["accept-encoding"]);
+	const language = req.headers["accept-language"];
+	const software = req.headers["user-agent"];
+	res.json({ipaddress: ip, language: language, software: software});
+});
+
 app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+	res.json({greeting: 'hello API'});
 });
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+	console.log('Your app is listening on port ' + listener.address().port);
 });
